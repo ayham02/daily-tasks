916f029f7cb9341e35586c0eb431f298aa607421
Ayham <mdayham21@gmail.com>
2026-06-18 17:29:28 +0530

    Day 4 quests

--- Day 4/backend/main.py
+++ Day 4/backend/main.py
@@ -1,11 +1,24 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/")
+class Student(BaseModel):
+    student_id: int
+    name: str
+    age: int
+    email: str
+    course: str
+    phone_number: str
+
+@app.get("/hello")
 async def root():
     return {"message": "Hello World"}
 
 @app.get("/student/{id}")
 async def get_student(id: int):
     return {"student_id": id}
+
+@app.post("/student/")
+async def create_student(student: Student):
+    return {"message": "Student created successfully", "student": student}
