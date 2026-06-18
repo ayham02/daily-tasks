bf90a042e3f3fe3f75aaf28d7d59451425ed0831
Ayham <mdayham21@gmail.com>
2026-06-18 18:18:35 +0530

    Day 4 and 5 quests

--- Day 4/backend/main.py
+++ Day 4/backend/main.py
@@ -1,15 +1,16 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, Field
 
 app = FastAPI()
 
 class Student(BaseModel):
     student_id: int
     name: str
-    age: int
-    email: str
+    age: int = Field(gt=18, description="Age must be greater than 18")
+    email: EmailStr 
     course: str
     phone_number: str
+
 
 @app.get("/hello")
 async def root():
@@ -22,3 +23,11 @@ async def get_student(id: int):
 @app.post("/student/")
 async def create_student(student: Student):
     return {"message": "Student created successfully", "student": student}
+
+@app.put("/student/{id}")
+async def update_student(id: int, student: Student):
+    return {"message": "Student updated successfully", "student_id": id, "student": student}
+
+@app.delete("/student/{id}")
+async def delete_student(id: int):
+    return {"message": "Student deleted successfully", "student_id": id}
